d0c0f23ba460b1276564f79269062eecba7bd177
Sajeevan R S <rssajeevan1010@gmail.com>
2023-11-06 13:59:49 +0530

    health check added

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+server.use('/health', (req, res) => {
+  res.json({ status: 'OK' });
+});
 server.use(router);
 
 server.listen(port);
